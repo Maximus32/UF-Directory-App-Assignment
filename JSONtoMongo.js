cb53f7fd5999ac274612bfc2267edee7e995e425
Maximus32 <dunevitz32@gmail.com>
2017-09-03 11:17:55 -0400

    Basic Functionality
    Added databse functionality and script to push listings to the Database
    
    TODO: queries

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,37 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
-  Make sure to save your DB's uri in the config file, then import it with a require statement!
- */
+
+// Imported modules/files
 var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
     config = require('./config');
 
-/* Connect to your database */
+// Connect to database
+mongoose.connect(config.db.uri) ;
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
- */
+// Call 'readFile' method, parse data, and add to the database
+fs.readFile('listings.json', 'utf8', function(err, raw_data) {
+	var index, listing ;
+	var listings = JSON.parse(raw_data).entries ;
+	
+	// Import all listings parsed from the file into the Database
+	for (index in listings) {
+		listing = listings[index] ;
+		
+		// Create new document
+		var newListing = Listing({
+			code: listing.code,
+			name: listing.name,
+			coordinates: listing.coordinates,
+			address: listing.address
+		}) ;
+		
+		// Save to database
+		newListing.save(function(err) {
+			if (err) throw err;
 
-
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
- */
+			console.log('Listing saved!') ;
+		});
+	}
+});
--- ListingSchema.js
+++ ListingSchema.js
@@ -2,14 +2,32 @@
 var mongoose = require('mongoose'), 
     Schema = mongoose.Schema;
 
-/* Create your schema */ 
+/* Define schema for Listing document */
 var listingSchema = new Schema({
-  /* your code here */
+	code: { type: String,  required: true, unique: true },
+	name: { type: String,  required: true }, 
+	coordinates: {
+		latitude: Number,
+		longitude: Number,
+	}, 
+	address: String,
+	
+	created_at: Date,
+	updated_at: Date
 });
 
-/* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
+/* 'Pre' method that sets the 'updated_at' field (and 'created_at' field if not already there) */
 listingSchema.pre('save', function(next) {
-  /* your code here */
+	var currentDate = new Date();
+	
+	// change the 'updated_at' field to current date
+	this.updated_at = currentDate;
+	
+	// if 'created_at' doesn't exist, add to that field
+	if (!this.created_at)
+		this.created_at = currentDate;
+	
+	next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
