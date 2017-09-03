84efbfbf1b03a8bbda872636c1343426197ebdde
Maximus32 <dunevitz32@gmail.com>
2017-09-03 10:06:06 -0400

    Test

--- ListingSchema.js
+++ ListingSchema.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose'), 
     Schema = mongoose.Schema;
 
-/* Create your schema */
+/* Create your schema */ 
 var listingSchema = new Schema({
   /* your code here */
 });
