c05bbdb687d8b5a3c8a9e5c9a896b556be972571
Maximus32 <dunevitz32@gmail.com>
2017-09-04 10:08:32 -0400

    Assignment Completed

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
-var http = require('http'), 
-    fs = require('fs'), 
-    url = require('url'),
-    port = 8080;
+var http	= require('http'), 
+	fs		= require('fs'), 
+	url		= require('url'),
+	port	= 8080 ;
 
-/* Global variables */
-var listingData, server;
+// Global variables
+var listingData ;
 
+// Create an HTTP requst handler to later be passed to a server
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
-
-  /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: explore the request object and its properties 
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
+	
+	// Create URL object from raw string obtained from request
+	var parsedUrl = url.parse(request.url) ;
+	
+	// If a "GET" HTTP request at the "/listings" resource path, write out the 'listingData' variable
+	// Else respond with a 404 error
+	if (request.method === 'GET' && parsedUrl.pathname === '/listings') {
+		response.write(listingData) ;
+	} else {
+		response.writeHead(404, { 'Content-Type' : 'text/plain' }) ;
+		response.write('Bad gateway error') ;
+	}
+	
+	// Close the response
+	response.end() ;
 };
 
+// Call 'readFile' method and call back an anonymous function to create a server with the
+// previously specified request handler
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-   */
+	
+	// Store data within "listings.json" into a global variable
+	listingData = data ;
+	
+	// Create server and listen at port '8080'
+	http.createServer(requestHandler).listen(port) ;
+	
+	console.log('server listening on: http://localhost:8080') ;
 });
+
+
